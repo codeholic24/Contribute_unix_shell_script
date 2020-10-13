8bcf7d4d9c976aa588b4a5d561d082cb7f659952
Vikas B Vooradi <72571474+codeholic24@users.noreply.github.com>
2020-10-14 01:13:40 +0530

    Update check_odd_or_even.sh

--- Scripts/check_odd_or_even.sh
+++ Scripts/check_odd_or_even.sh
@@ -5,6 +5,12 @@
 # Parameters  : <InputNumber>
 # Purpose     : Check the given number is even or odd 
 
+# Scenario 
+# case 1 : if number is positive then find it is even or odd
+# case 2 : if number is negative convert it to positive and find it is even or odd
+# case 3 : if its a floating number , maybe negative [-2.5] or postive [4.5] exit the process with a msg
+# Note   : This all sceanrio should be covered in only one script 
+
 check_odd_or_even()
 {
    # Intializing the value 
@@ -31,6 +37,7 @@ check_odd_or_even()
    exit 0
    fi
 
+   check_isInteger_or_decimal $getNumber
    check_positive_number $getNumber
    
 }
@@ -42,9 +49,9 @@ calculate_it()
  
     if [ $rem -eq 0 ]
     then
-        echo "Number : $getNum is even number"
+        echo "Number : $getNum is a even number"
     else
-        echo "Number : $getNum is odd number"
+        echo "Number : $getNum is a odd number"
     fi
 }
 
@@ -61,20 +68,87 @@ check_positive_number()
     else      
          echo "It is not a positive number"
          swap_negative_to_positive $getNum
+         exit 1
     fi
 }
 
-# swap_negative_to_positive will swap the number from negative to positive
-swap_negative_to_positive()
-{
+ swap_negative_to_positive will swap the number from negative to positive
+ swap_negative_to_positive()
+ {
     if [ $getNum -lt 0 ]
     then 
         echo "Converting negative number to postive number"
         getNum=`echo $getNum | tr -d '-'`
         calculate_it $getNum
     fi    
+ }
+
+# Check value is decimal or not
+check_isInteger_or_decimal()
+{
+    real_num=$getNumber
+    swap_it=`echo $getNumber | tr -d '-'` 
+    getNumber=$swap_it
+        
+    echo "$getNumber" | grep "^[0-9]*[.][0-9]*$"  
+    status=`echo $?`
+    if [[ $status == 0 ]]
+    then
+        echo "It is float number .. cannot continue : $real_num"
+        echo "The script will not process if it's a decimal number or having negative sign to decimal number"
+        exit 1
+    else
+        echo "No issue with the provided number : $getNumber"
+    fi
 }
 
 # Calling function : 1 parameters is intialized 
-# Note : If you want to test just replace the 1 parameters with your parameters 
-check_odd_or_even -5
+# Note : If you want to test just replace the parameters with your parameters 
+check_odd_or_even 2.0
+
+#Output 
+
+#parameter is passsed as like following :  check_odd_or_even 1
+#$ sh check_odd_or_even.sh
+#  Number  : 1 : is defined
+#  Count of parameters matched 1 : 1
+#  No issue with the provided number : 1
+#  It is a positive number
+#  Calling function to calculate even or odd
+#  Number : 1 is a odd number
+
+#parameter is passsed as like following :  check_odd_or_even 2
+#$ sh check_odd_or_even.sh
+#  Number  : 2 : is defined
+#  Count of parameters matched 1 : 1
+#  No issue with the provided number : 2
+#  It is a positive number
+#  Calling function to calculate even or odd
+#  Number : 2 is a even number
+
+#parameter is passsed as like following :  check_odd_or_even -2
+#$ sh check_odd_or_even.sh
+#  Number  : 2 : is defined
+#  Count of parameters matched 1 : 1
+#  No issue with the provided number : 2
+#  It is a positive number
+#  Calling function to calculate even or odd
+#  Number : 2 is a even number
+
+#parameter is passsed as like following :  check_odd_or_even -2.0
+#$ sh check_odd_or_even.sh
+# check_odd_or_even.sh: line 69: swap_negative_to_positive: command not found
+# Number  : -2.0 : is defined
+# Count of parameters matched 1 : 1
+# 2.0
+# It is float number .. cannot continue : -2.0
+# The script will not process if it's a decimal number or having negative sign to decimal number
+
+#parameter is passsed as like following :  check_odd_or_even 2.0
+#$ sh check_odd_or_even.sh
+#  check_odd_or_even.sh: line 69: swap_negative_to_positive: command not found
+#  Number  : 2.0 : is defined
+#  Count of parameters matched 1 : 1
+#  2.0
+#  It is float number .. cannot continue : 2.0
+#  The script will not process if it's a decimal number or having negative sign to decimal number
