c4d6143944be24c3011cf313370ef3b410914a63
Vikas B Vooradi <72571474+codeholic24@users.noreply.github.com>
2020-10-20 12:44:12 +0530

    Update Linear_search.sh

--- Scripts/Linear_search.sh
+++ Scripts/Linear_search.sh
@@ -10,6 +10,10 @@ Linear_Search()
 
    # Intializing the value 
    getNumber=$1
+   array_size=$2
+
+   # stored in variable to make use of it in function as an when required
+   snumber=$getNumber 
 
    # checking value entered is null or not  
    [ -z $getNumber ]
@@ -17,69 +21,237 @@ Linear_Search()
 
    if [ $status -ne 0 ]
    then
-       echo "Number  : $getNumber : is defined"
+       newLine  
+       echo "Value    : $getNumber : is defined"
+
+       # Function : check_is_number_or_not is called to check the entered value is number or not
+       check_is_number_or_not $getNumber  
    else
-       echo "Number  : is not defined...empty" 
+       newLine
+       echo "Value    : is not defined...empty" 
+       exit 0 
+   fi 
+
+   # checking value entered is null or not  
+   [ -z $array_size ]
+   status=$?
+
+   if [ $status -ne 0 ]
+   then
+       newLine
+       echo "Value   : $array_size : is defined"
+       
+       # To make function reusable , I have just taken the $array_size to a variable : getNumber
+       # Becasue the function expecting the getNumber variable 
+       getNumber=$array_size 
+
+       # Function : check_is_number_or_not is called to check the entered value is number or not
+       check_is_number_or_not $getNumber  
+   else
+       newLine
+       echo "Value    : is not defined...empty" 
        exit 0 
    fi 
 
    # checking the parameters entered are matching or not 
-   if [ "$#" -eq 1 ] 
+   if [ "$#" -eq 2 ] 
    then
-   echo "Count of parameters matched `echo $#` : 1 " 
+   newLine
+   echo "Count of parameters matched `echo $#` : 2 " 
    else 
-   echo "Illegal number of parameters passed.Expected parameters are 1 ..try again"
+   newLine
+   echo "Illegal number of parameters passed.Expected parameters are 2 ..try again"
    exit 0
    fi
+   
+   # Calling Function : readInput to check the entered array values are vaild or not , if not the script with exit
+   readInput $array_size 
 
-   search_number_in_array $getNumber
- 
+   # Calling Function : To search the given value in the array list 
+   search_Number $snumber
+
 }
 
-search_number_in_array()
+# Function : check_is_number_or_not is created the check the given value is a Number or Not 
+check_is_number_or_not()
 {
+   re='^[0-9]+$'
+   if ! [[ $getNumber =~ $re ]] ; then
+   newLine
+   echo "Error : $getNumber is Not a number  " >&2; exit 1
+   fi
+}
 
-   # Defined array
-   arr=([0]=2 [1]=5 [2]=19 [3]=20 [4]=4 [5]=54) 
-   strnumber=${#arr[@]}
-   found=0 
+# To Print the echo statement on a new line 
+newLine()
+{
+    echo ""
+}
+
+# It reading array values as an Input from User 
+readInput()
+{
+   dynamic_array=()
+
+   newLine 
+   echo "Please enter $array_size array values : "
    
-   # Checking its number or not 
-   check_is_number_or_not $getNumber
+   for (( i=0 ; i<=$array_size ; i++ ))
+   { 
+      newLine 
+      echo -n "ENTER ARRAY NUMBER - $i : " ; read array_number
+      getNumber=$array_number
+      check_is_number_or_not $getNumber
+      dynamic_array+=("$getNumber")
+   }
+   
+}
 
-   # Printing count of elements present in an array
-   echo "Count of elements present : ${#arr[@]}"
+# It searches the entered values in the Array list to find if its exist or Not 
+search_Number()
+{
+   newLine 
+   # Print total size of an array  
+   echo Total size of Array : ${#dynamic_array[@]}
+   
+   newLine
+   # Print all array values 
+   echo Array Values        : ${dynamic_array[@]}
 
+   # Stores the array size in variable    
+   strnumber=${#dynamic_array[@]}
+    
+   # Initial state setting it as zero : Not found 
+   found=0
+
+   # Linear search : Logic defined , It iterates all the values one by one and find for values exist in an array or not 
    for (( i=0 ; i<=$strnumber ; i++  ))
    {
-       getval=`echo ${arr[i]}`
-       if [ "$getval" = "$getNumber" ]
+       getval=`echo ${dynamic_array[i]}`
+       if [ "$getval" = "$snumber" ]
        then 
-           echo "Value found : $getNumber"
+           newLine
+           echo "Value found : $snumber in array list : ${dynamic_array[@]} "
            found=1
            break
        fi       
    }
 
+   #It returns status , fail or success
    if [ $found -eq 0  ]
    then
-       echo "Number does not exist in an array"
-   fi    
+       newLine
+       echo "Number : $snumber does not exist in an array"
+   fi 
 
 }
 
-check_is_number_or_not()
-{
-   re='^[0-9]+$'
-   if ! [[ $getNumber =~ $re ]] ; then
-   echo "error: Not a number" >&2; exit 1
-   fi
-}
 
 echo -e "\nLINEAR SEARCH"
-echo ""
+
+newLine
 
 #Reading number which need to search in an array 
-echo -n "Please enter your number : "; read number
-Linear_Search $number
+echo -n "Please enter your search number : "; read search_number
+newLine
+echo -n "Please enter size of an array   : "; read array_size
 
+
+# The below defined function takes two paramters parameter 1 : search number & parameter 2 : array size 
+# search number :  It contains a number which we want to find in the array list present or not 
+# array size    :  It contain size of an array ie : number of elements will be their in an array 
+
+Linear_Search $search_number $array_size
+
+# -----------------------------------------------------------------
+
+# Output 1 :
+# $ sh Linear_search.sh
+
+# LINEAR SEARCH
+# Please enter your search number : 55
+# Please enter size of an array   : 4
+# Value    : 55 : is defined
+# Value   : 4 : is defined
+# Count of parameters matched 2 : 2
+# Please enter 4 array values :
+
+# ENTER ARRAY NUMBER - 0 : 23
+# ENTER ARRAY NUMBER - 1 : 45
+# ENTER ARRAY NUMBER - 2 : 55
+# ENTER ARRAY NUMBER - 3 : 23
+# ENTER ARRAY NUMBER - 4 : 89
+
+# Total size of Array : 5
+# Array Values : 23 45 55 23 89
+# Value found : 55 in array list : 23 45 55 23 89
+
+# -----------------------------------------------------------------
+
+# Output 2 :
+# $ sh Linear_search.sh
+
+# LINEAR SEARCH
+# Please enter your search number : r
+# Please enter size of an array   : 5
+# Value    : r : is defined
+# Error : r is Not a number
+
+
+# Output 3 :
+# $ sh Linear_search.sh
+
+# LINEAR SEARCH
+# Please enter your search number : 45
+# Please enter size of an array   : f
+# Value    : 45 : is defined
+# Value   : f : is defined
+# Error : f is Not a number
+
+# -----------------------------------------------------------------
+
+# Output 4 :
+# $ sh Linear_search.sh
+
+# LINEAR SEARCH
+# Please enter your search number : 77
+# Please enter size of an array   : 3
+# Value    : 77 : is defined
+# Value   : 3 : is defined
+
+# Count of parameters matched 2 : 2
+
+# Please enter 3 array values :
+
+# ENTER ARRAY NUMBER - 0 : 23
+# ENTER ARRAY NUMBER - 1 : 45
+# ENTER ARRAY NUMBER - 2 : 67
+# ENTER ARRAY NUMBER - 3 : dd
+# Error : dd is Not a number
+
+# -----------------------------------------------------------------
+# Output 3 :
+
+# $ sh Linear_search.sh
+
+# LINEAR SEARCH
+
+# Please enter your search number : 32
+# Please enter size of an array   : 3
+# Value    : 32 : is defined
+# Value   : 3 : is defined
+
+# Count of parameters matched 2 : 2
+
+# Please enter 3 array values :
+
+# ENTER ARRAY NUMBER - 0 : 22
+# ENTER ARRAY NUMBER - 1 : 56
+# ENTER ARRAY NUMBER - 2 : 78
+# ENTER ARRAY NUMBER - 3 : 90
+
+# Total size of Array : 4
+# Array Values : 22 56 78 90
+# Number : 32 does not exist in an array
+
+# -----------------------------------------------------------------
